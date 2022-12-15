5cea12e4d0c7e18101441bd2c6ccec6373254a1e
AbhyasKanaujia <44172451+AbhyasKanaujia@users.noreply.github.com>
2022-12-15 16:54:43 +0530

    practice scroll indicator

--- practice/page-scroll-indicator/script.js
+++ practice/page-scroll-indicator/script.js
@@ -3,7 +3,14 @@ window.onscroll = function () {
 }
 
 function myFunction() {
+  // get how far the user has scrolled
   var winScroll = document.body.scrollTop || document.documentElement.scrollTop
-  console.log(winScroll)
-  console.log(document.body)
+  // get the full height of the page
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight
+  // calculates what percentage the user has scrolled
+  var scrolled = (winScroll / height) * 100
+  // Set the width of the progress-bar to that %
+  document.getElementById('myBar').style.width = `${scrolled}%`
 }
